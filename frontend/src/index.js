1c7e5724098440b0c1aa29f86653849340da3d60
Anastasios3 <117446378+Anastasios3@users.noreply.github.com>
2024-06-04 17:54:56 +0300

    feat: Update App component to use React Router for routing
    This commit modifies the App component in the frontend/src/App.js file to use React Router for routing. It imports the Outlet component from react-router-dom and replaces the HomeScreen component with the Outlet component within the Container component.
    
    By using React Router, we can implement dynamic routing in our application, allowing users to navigate between different screens based on the URL. This improves the overall user experience and makes our application more flexible and scalable.
    
    Co-authored-by: [Author Name]

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import Homescreen from "./screens/HomeScreen";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<Homescreen />} />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
