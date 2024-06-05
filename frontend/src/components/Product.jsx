b46dd92acd508f1116a53e46621dfe0c29f0d33f
Anastasios3 <117446378+Anastasios3@users.noreply.github.com>
2024-06-05 20:16:28 +0300

    feat: Update Product component to use React Router for navigation

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,19 +1,29 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
 import "./Product.css";
+
 const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded product-card">
-      <a href={`/Product/${product._id}`}>
+      <Link to={`/Product/${product._id}`}>
         <Card.Img src={product.image} variant="top" className="product-image" />
-      </a>
+      </Link>
 
       <Card.Body>
-        <a href={`/Product/${product._id}`}>
+        <Link to={`/Product/${product._id}`}>
           <Card.Title as="div" className="product-title">
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
+
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
 
         <Card.Text as="h3" className="product-price">
           ${product.price}
