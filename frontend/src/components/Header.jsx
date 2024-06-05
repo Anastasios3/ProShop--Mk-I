cef585c1e86309e1d20a699546fb44741a7b0dfd
Anastasios3 <117446378+Anastasios3@users.noreply.github.com>
2024-06-05 19:39:41 +0300

    feat: Update Header component to use React Router for navigation
    This commit updates the Header component in the frontend/src/components/Header.jsx file to use React Router for navigation. The code changes include replacing the Nav.Link components with LinkContainer components from react-router-bootstrap package. This allows for seamless integration of React Router with Bootstrap components.
    
    By implementing React Router for navigation, we improve the user experience and make the application more flexible and scalable. Users can now navigate to different pages within the application without the need for full page reloads.
    
    Co-authored-by: [Author Name]

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -9,25 +9,31 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">
-            <img
-              src={logo}
-              alt="ProShop Mk I"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />
-            ProShop Mk I
-          </Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img
+                src={logo}
+                alt="ProShop Mk I"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+              />
+              ProShop Mk I
+            </Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart">
-                <FaShoppingCart /> Cart
-              </Nav.Link>
-              <Nav.Link href="/login">
-                <FaUser /> Sign In
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart /> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/signin">
+                <Nav.Link>
+                  <FaUser /> Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
