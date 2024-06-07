e05ea8316629e160a1dcba5698f20202c61cb293
Anastasios3 <117446378+Anastasios3@users.noreply.github.com>
2024-06-07 23:34:00 +0300

    chore: Update server.js and ProductScreen.jsx
    The code changes in this commit update the server.js file to use port 8000 instead of the previous port for consistency. Additionally, the ProductScreen.jsx file is modified to fetch a single product using axios instead of relying on the local products array.
    
    This commit improves the codebase by ensuring consistent port usage and enhancing the functionality of the ProductScreen component to retrieve data dynamically from the backend server.

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import products from "./data/products.js";
-const port = process.env.PORT || 5000;
+
+const port = process.env.PORT || 8000; // Updated the port to 8000 for consistency
 
 const app = express();
 
@@ -13,9 +14,10 @@ app.get("/", (req, res) => {
 app.get("/api/products", (req, res) => {
   res.json(products);
 });
+
 app.get("/api/products/:id", (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
-  res.json(products);
+  res.json(product); // Corrected the response to send the single product
 });
 
 app.listen(port, () => {
