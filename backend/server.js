bd65fa4269529cab21e702524beddd1935f870dd
Anastasios3 <117446378+Anastasios3@users.noreply.github.com>
2024-06-07 16:59:57 +0300

    chore: Add API endpoints for retrieving product data

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,14 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(products);
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
