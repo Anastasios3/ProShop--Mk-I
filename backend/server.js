7b5e50506a742cc798475a11473410620268806f
Anastasios3 <117446378+Anastasios3@users.noreply.github.com>
2024-06-07 16:43:43 +0300

    chore: Update npm dependency to latest stable version

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+import express from "express";
+const port = 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
