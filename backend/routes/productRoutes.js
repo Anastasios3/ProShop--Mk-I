b455a04d24fabd09c9e93c35218a3150bdebe4ea
Anastasios3 <117446378+Anastasios3@users.noreply.github.com>
2024-06-09 18:35:59 +0300

    refactor: Use asyncHandler middleware in productRoutes.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,38 +1,43 @@
 import express from "express";
 const router = express.Router();
-import asyncHandler from "../middleware/asyncHandler.js";
-import Product from "../models/productModel.js";
+import {
+  getProducts,
+  getProductById,
+} from "../controllers/productController.js";
 
-// router --1
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    try {
-      const products = await Product.find({});
-      res.json(products);
-    } catch (error) {
-      res.status(500).json({ message: "Server Error" });
-    }
-  })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
-// router --2
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    try {
-      const product = await Product.findById(req.params.id);
-      if (product) {
-        res.json(product);
-      } else {
-        res.status(404);
-        throw new Error("Resource not found");
-      }
-    } catch (error) {
-      res.status(500).json({ message: "Server Error" });
-    }
-  })
-);
+// // router --1
+// router.get(
+//   "/",
+//   asyncHandler(async (req, res) => {
+//     try {
+//       const products = await Product.find({});
+//       res.json(products);
+//     } catch (error) {
+//       res.status(500).json({ message: "Server Error" });
+//     }
+//   })
+// );
+
+// // router --2
+// router.get(
+//   "/:id",
+//   asyncHandler(async (req, res) => {
+//     try {
+//       const product = await Product.findById(req.params.id);
+//       if (product) {
+//         res.json(product);
+//       } else {
+//         res.status(404);
+//         throw new Error("Resource not found");
+//       }
+//     } catch (error) {
+//       res.status(500).json({ message: "Server Error" });
+//     }
+//   })
+// );
 
 export default router;
 // Path: backend/routes/productRoutes.js
