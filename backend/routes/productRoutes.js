f4c87c2acdf0c3cca7786c3aa18ca88a0db22a9d
Anastasios3 <117446378+Anastasios3@users.noreply.github.com>
2024-06-09 17:35:24 +0300

    chore: Update server.js to use product routes

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,30 @@
+import express from "express";
+const router = express.Router();
+import products from "../data/products.js";
+
+// router --1
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+// router --2
+router.get("/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+export default router;
+// Path: backend/routes/productRoutes.js
--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
-const port = process.env.PORT || 8000; // Updated the port to 8000 for consistency
+import Product from "./models/productModel.js";
+import productRoutes from "./routes/productRoutes.js";
+const port = process.env.PORT || 8000;
 
-connectDB(); // Added the call to connectDB
+connectDB();
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product); // Corrected the response to send the single product
-});
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
