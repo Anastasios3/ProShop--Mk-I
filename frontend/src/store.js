77457bc531dd5616344448d8e853e7d52d9d38a0
Anastasios3 <117446378+Anastasios3@users.noreply.github.com>
2024-06-09 21:12:55 +0300

    refactor: Update BASE_URL and remove unused code
    Update the BASE_URL constant in constants.js to an empty string and remove the commented out code. This change ensures that the application uses the correct base URL for API requests. Additionally, unnecessary code is removed for better code readability and maintainability.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,15 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice";
+import { productsApiSlice } from "./slices/productsApiSlice";
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    [productsApiSlice.reducerPath]: productsApiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiSlice.middleware),
+    getDefaultMiddleware().concat(
+      apiSlice.middleware,
+      productsApiSlice.middleware
+    ),
   devTools: true,
 });
 
 export default store;
-
-// Path: frontend/src/store.js
