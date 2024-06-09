92430fe0f3534ef012fbbe9c25772ab9b43a26df
Anastasios3 <117446378+Anastasios3@users.noreply.github.com>
2024-06-09 18:08:54 +0300

    feat: Improve error handling in productRoutes.js
    Refactor the route handlers in productRoutes.js to use the asyncHandler middleware. This improves error handling and simplifies the code by removing repetitive try-catch blocks.

---
+++ backend/controllers/productController.js
@@ -0,0 +1,17 @@
+import asyncHandler from "express-async-handler";
+import Product from "../models/productModel.js";
+
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
+
+const getProductbyId = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Resource not found");
+  }
+});
