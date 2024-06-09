391f6d0134af754aeaa5cbf5be1ba14c19a7d76a
Anastasios3 <117446378+Anastasios3@users.noreply.github.com>
2024-06-09 20:23:13 +0300

    refactor: Update store.js to use apiSlice middleware
    Update the store.js file to include the apiSlice middleware. This allows for handling API requests and responses in a more efficient and standardized way. The apiSlice.reducerPath and apiSlice.middleware are added to the store's reducer and middleware configurations respectively. This change improves the overall functionality and maintainability of the application.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./slices/apiSlice";
 
 const store = configureStore({
   reducer: {
-    /* reducers */
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 export default store;
