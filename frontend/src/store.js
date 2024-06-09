27d7643e28c007dc69cb6c6ba6326a684ed922b4
Anastasios3 <117446378+Anastasios3@users.noreply.github.com>
2024-06-09 20:29:03 +0300

    refactor: Enable devTools in store.js
    Enable the devTools option in the store.js file to allow for better debugging and monitoring of the application state. This change adds the "devTools: true" configuration to the store's middleware. It improves the development experience by providing access to the Redux DevTools extension for inspecting and manipulating the state.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,6 +7,7 @@ const store = configureStore({
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
 });
 
 export default store;
