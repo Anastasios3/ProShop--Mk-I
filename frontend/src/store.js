0f261d425a359ccdc161d30a078c62bd43ae037a
Anastasios3 <117446378+Anastasios3@users.noreply.github.com>
2024-06-10 13:26:26 +0300

    feat: Add cartSlice to store.js
    The code changes in store.js add the cartSlice to the store's reducer configuration. This allows for managing the state related to the shopping cart functionality. By including the cartSliceReducer, the application can handle cart-related actions and update the cart state accordingly.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice";
 import { productsApiSlice } from "./slices/productsApiSlice";
+import cartSliceReducer from "./slices/cartSlice";
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     [productsApiSlice.reducerPath]: productsApiSlice.reducer,
+    cart: cartSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
